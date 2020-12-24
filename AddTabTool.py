3137e8b3457ebb99e47d55a061de9291a46ed74d
Brian Mc Donald <brian.mcdonald01@gmail.com>
2020-12-24 11:39:54 +0000

    removed 1 scripts and updated README

--- AddTabTool.py
+++
@@ -1,8 +0,0 @@
-import xlwings as xw
-
-temp = xw.Book(r'r3template.xlsx')
-r3template = wb.sheets['Round 3']
-
-agh = xw.Book(r'testresponses/GHRP_IOM_Indicators_Data_Collection_Template - Afghanistan.xlsx')
-new_wb.sheets['Round 3'] = r3template
-new_wb(r'new.xlsx')
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# DataCleaningTool
 # GHRP-tools
+This script is for compiling responses across a number of Excels.
